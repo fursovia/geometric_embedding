f0f832990ac977465a8ea43dea0d290b83470d85
fursovia <CS9mwwux>
2018-12-23 05:04:22 +0300

    improved the algorithm

--- emb_path.py
+++
@@ -1,5 +0,0 @@
-glove_6B_50d_path = "data/glove/glove.6B.50d.txt"
-glove_6B_100d_path = "data/glove/glove.6B.100d.txt"
-glove_6B_200d_path = "data/glove/glove.6B.200d.txt"
-glove_6B_300d_path = "data/glove/glove.6B.300d.txt"
-lexvec_7B_300d_path = "data/lexvec/lexvec.enwiki+newscrawl.300d.W.pos.vectors"
--- gem.py
+++ gem.py
@@ -32,11 +32,14 @@ def modified_gram_schmidt_qr(A):
 
 class SentenceEmbedder:
 
-    def __init__(self, sentences_raw: List[str], embedding_matrix: np.ndarray, vocab: dict, ngrams: int = 1):
+    def __init__(self,
+                 sentences_raw: List[str],
+                 embedding_matrix: np.ndarray,
+                 vocab: dict) -> None:
+
         self.vocab = vocab
         self.sentences_raw = sentences_raw
         self.sentences = []
-        self.ngrams = ngrams
 
         for sent in self.sentences_raw:
             self.sentences.append(sentence_to_indexes(sent, self.vocab))
@@ -45,16 +48,29 @@ class SentenceEmbedder:
         self.emb_dim = self.embedding_matrix.shape[1]
         self.singular_values = None
 
-    def gem(self, window_size: int = 7, k: int = 45, h: int = 17, sigma_power: int = 3):
+    def gem(self,
+            window_size: int = 7,
+            k: int = 45,
+            h: int = 17,
+            sigma_power: int = 3,
+            ngrams: int = 1) -> np.ndarray:
         """
-        Runs the GEM algorithm
+        Runs GEM algorithm. See the paper for detailed explanation of arguments.
+        Args:
+            window_size: int, the size of the window
+            k: int, number of singular values
+            h: int, number of principal vectors
+            sigma_power: int, power for the function in (7)
+            ngrams: int, n-grams size
         Returns:
             sentence_embeddings: shape [n, d]
         """
         X = np.zeros((self.emb_dim, len(self.sentences)))
+        embedded_sentences = []
 
-        for i, sent in enumerate(tqdm(self.sentences)):
-            embedded_sent = inds_to_embeddings(sent, self.embedding_matrix, self.ngrams)
+        for i, sent in enumerate(self.sentences):
+            embedded_sent = inds_to_embeddings(sent, self.embedding_matrix, ngrams)
+            embedded_sentences.append(embedded_sent)
             U, s, Vh = np.linalg.svd(embedded_sent, full_matrices=False)
             X[:, i] = U.dot(s ** sigma_power)
 
@@ -64,34 +80,42 @@ class SentenceEmbedder:
         s = s[:k]
 
         C = np.zeros((self.emb_dim, len(self.sentences)))
-        for j, sent in enumerate(tqdm(self.sentences)):
-            embedded_sent = inds_to_embeddings(sent, self.embedding_matrix, self.ngrams)
+        for j, sent in enumerate(self.sentences):
+            embedded_sent = embedded_sentences[j]
             order = s * np.linalg.norm(embedded_sent.T.dot(D), axis=0)
             toph = order.argsort()[::-1][:h]
             alpha = np.zeros(embedded_sent.shape[1])
+
             for i in range(embedded_sent.shape[1]):
                 window_matrix = self._context_window(i, window_size, embedded_sent)
                 Q, R = modified_gram_schmidt_qr(window_matrix)
                 q = Q[:, -1]
                 r = R[:, -1]
                 alpha_n = np.exp(r[-1] / (np.linalg.norm(r, ord=2, axis=0)) + 1e-18)
-                alpha_s = r[-1] / window_matrix.shape[1]  # (2 * window_size + 1)
+                alpha_s = r[-1] / window_matrix.shape[1]
                 alpha_u = np.exp(-np.linalg.norm(s[toph] * (q.T.dot(D[:, toph]))) / h)
                 alpha[i] = alpha_n + alpha_s + alpha_u
+
             C[:, j] = embedded_sent.dot(alpha)
             C[:, j] = C[:, j] - D.dot(D.T.dot(C[:, j]))
 
         sentence_embeddings = C.T
-        return sentence_embeddings, self.singular_values
+        return sentence_embeddings
 
-    def mean_embeddings(self):
+    def mean_embeddings(self) -> np.ndarray:
+        """
+        Averages embeddings of words to get a sentence representation.
+        Returns:
+            sentence_embeddings: shape [n, d]
+        """
         C = np.zeros((self.emb_dim, len(self.sentences)))
 
         for i, sent in enumerate(self.sentences):
-            embedded_sent = inds_to_embeddings(sent, self.embedding_matrix, self.ngrams)
+            embedded_sent = inds_to_embeddings(indexes=sent, emb_matrix=self.embedding_matrix)
             C[:, i] = np.mean(embedded_sent, axis=1)
 
-        return C.T, None
+        sentence_embeddings = C.T
+        return sentence_embeddings
 
     def _context_window(self, i: int, m: int, embeddings: np.ndarray) -> np.ndarray:
         """
